2fb4a6fae36ebca318f6584dfd0495b46ca1060f
ASHISH DASH <ashibabi2007@gmail.com>
2026-09-16 22:39:46 +0530

    Time: 0 ms (100%), Space: 44.3 MB (38.5%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -2,9 +2,7 @@ class Solution {
     public int findPeakElement(int[] nums) {
         int l=0;
         int r=nums.length-1;
-        if(nums.length==1){
-                return 0;
-            }
+        
         while(l<r){
             if (nums[l+1]<nums[l]){
                 return l;
