d50b854e468a1fa9ef08c992f666c3f65cd3304c
ASHISH DASH <ashibabi2007@gmail.com>
2026-09-16 20:03:08 +0530

    Time: 0 ms (100%), Space: 42.1 MB (56.74%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int l=1;
+        int r=num;
+        
+        if(num==0 || num==1){
+            return true;
+        }
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if (mid==num/mid){
+                if(mid*mid==num){
+                return true;
+            }}
+            if (mid<num/mid){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return false;
+    }
+}
