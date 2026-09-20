ba45572eea194c37495c57ac5fb65a56cabf168d
ASHISH DASH <ashibabi2007@gmail.com>
2026-09-20 15:34:59 +0530

    Time: 47 ms (16.41%), Space: 90.6 MB (68.01%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        Arrays.sort(nums);
+        int ans=0;
+        if (nums.length<2){
+            return 0;
+        }
+        for (int i=1;i<nums.length;i++){
+            ans=Math.max(ans,nums[i]-nums[i-1]);
+        }
+        return ans;
+    }
+}
