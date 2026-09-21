f12fbfc741c6127e54a47319070141b6f4933470
ASHISH DASH <ashibabi2007@gmail.com>
2026-09-21 09:58:53 +0530

    Time: 7 ms (21.66%), Space: 45.1 MB (75.27%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,20 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
-
-        int left = 0;
-        int right = 1;
-
-        while (right < nums.length) {
-            if (right + 1 < nums.length && nums[left] == nums[right] && nums[right] == nums[right + 1]) {
-                left = left + 3;
-                right = right + 3;
+        int l=0;
+        int r=1;
+        while(r<nums.length){
+            if (r+1<nums.length-1 && nums[l]==nums[r] && nums[r]==nums[r+1] ){
+                l+=3;
+                r+=3;
             }
-            else {
-                return nums[left];
+            else{
+                return nums[l];
             }
-        }
-
-        return nums[left];
+        }return nums[l];
     }
 }
