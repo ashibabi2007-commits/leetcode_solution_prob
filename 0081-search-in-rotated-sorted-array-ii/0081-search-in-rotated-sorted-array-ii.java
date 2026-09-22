138d554fd450d58490cb92dd68b0c42505930991
ASHISH DASH <ashibabi2007@gmail.com>
2026-09-22 17:03:27 +0530

    Time: 0 ms (100%), Space: 45 MB (65.5%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        
         int l=0;
         int r=nums.length-1;
         while(l<=r){
-            if (nums[l]==target){
+            if(nums[l]==target){
                 return true;
             }
             l++;
-        }
-        return false;
+        }return false;
+    
     }
 }
