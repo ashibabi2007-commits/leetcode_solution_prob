045e3855df7bde2d6fcb5b9c0177f499162cfefa
ASHISH DASH <ashibabi2007@gmail.com>
2026-09-23 16:40:16 +0530

    Time: 4 ms (15.97%), Space: 44 MB (16.31%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -14,3 +14,16 @@ class Solution {
         }
     }
 }
+// //class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int right=0;
+//         while (right<n){
+//             if(nums2[right]!=0){
+//                 nums1[m]=nums2[right];
+//                 m++;
+//             }
+//             right++;
+//         }
+//         Arrays.sort(nums1);
+//     }
+// }
